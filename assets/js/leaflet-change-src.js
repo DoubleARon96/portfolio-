38d544d967a18bb8085e0cf2ac3a7f3acde760da
Aaron Storey <aaron.storey@hotmail.co.uk>
2026-05-02 23:01:30 +0100

    Feat: i have added a back up thats an embeded one if the object fails

--- assets/js/leaflet-change-src.js
+++ assets/js/leaflet-change-src.js
@@ -1,6 +1,8 @@
 function changePDF(pdfPath) {
     const viewer =document.getElementById('pdf-link');
     const link =document.getElementById('pdf-link-download')
+    const embed = document.getElementById('pdf-embed');
+     embed.src = pdfPath;
      viewer.data = pdfPath;
      link.href = pdfPath;
     }
